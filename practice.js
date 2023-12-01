203d49c39e16ab5de364202f30b7ff50da63aa07
ridashafqaat <ridashafqat282@gmail.com>
2023-12-01 10:51:39 +0500

    adding another changingg

--- practice.js
+++ practice.js
@@ -86,4 +86,22 @@ document.addEventListener("DOMContentLoaded", function(){
 
 //  active link
 
+// let sections = document.querySelectorAll('section');
+// let navLinks = document.querySelectorAll('header nav li a');
+
+// window.onscroll = () => {
+//     sections.forEach(sec => {
+//         let top = window.scrollY;
+//         let offset = sec.offsetTop;
+//         let height = sec.offsetHeight;
+//         let id = sec.getAttribute('id');
+
+//         if(top >= offset && top < offset + height) {
+//             navLinks.forEach(links => {
+//                 links.classList.remove('active');
+//                 document.querySelector('header nav li a [href*=' + id + ']').classList.add('active');
+//             });
+//         };
+//     });
+// };
 
